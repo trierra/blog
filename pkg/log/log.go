5b69ba9428bcd4ccfbb9d8fd902eb9ffdf1a890b
trierra <oksana@portworx.com>
2019-09-29 20:42:21 -0700

    add log code for post https://trierra.dev/how-to-configure-a-golang-logger-logrus-for-production

---
+++ pkg/log/log.go
@@ -0,0 +1,32 @@
+package log
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	logrus.SetReportCaller(true)
+	formatter := &logrus.TextFormatter{
+		TimestampFormat:        "02-01-2006 15:04:05", // the "time" field configuration
+		FullTimestamp:          true,
+		DisableLevelTruncation: true, // log level field configuration
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			// this function is required when you want to introduce your custom format.
+			// In my case I wanted file and line to look like this `file="engine.go:141`
+			// but f.File provides a full path along with the file name.
+			// So in `formatFilePath()` function I just trimmed everything before the file name
+			// and added a line number in the end
+			return "", fmt.Sprintf("%s:%d", formatFilePath(f.File), f.Line)
+		},
+	}
+	logrus.SetFormatter(formatter)
+}
+
+func formatFilePath(path string) string {
+	arr := strings.Split(path, "/")
+	return arr[len(arr)-1]
+}
